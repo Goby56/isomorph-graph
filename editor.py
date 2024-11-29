bb9f7d25b2b3d5e01de61fe13598054e208be010
Karl Rosengren <karl.rosengren.kr@gmail.com>
2024-11-29 21:27:27 +0100

    Load graphs and remove nodes

--- editor.py
+++ editor.py
@@ -1,4 +1,4 @@
-import pygame, sys, math, os
+import pygame, sys, math, os, random
 import numpy as np
 import pandas as pd
 
@@ -17,17 +17,22 @@ selected_node = -1
 def get_graph_filenames():
     return [name for name in os.listdir(GRAPH_DIR) if os.path.isfile(os.path.join(GRAPH_DIR, name))]
 
-def handle_click(mouse_pos, selected_node, ad_matrix):
+def handle_click(mouse_button, mouse_pos, selected_node, ad_matrix):
+    right_click = mouse_button == 3 
     x, y = mouse_pos
-    i = node_collision(x, y)
-    if i == -1:
-        nodes.append((x, y))
+    collided_node = node_collision(x, y)
+    if collided_node != -1:
+        if right_click:
+            remove_node(collided_node)
+            return -1
+        if selected_node == -1:
+            return collided_node
+        create_connection(selected_node, collided_node, ad_matrix)
+        
+    if collided_node == -1 and not right_click:
+        nodes.append([x, y])
         return selected_node
-    
-    if selected_node == -1:
-        return i
 
-    create_connection(selected_node, i, ad_matrix)
     return -1 
     
 def node_collision(x, y):
@@ -38,8 +43,18 @@ def node_collision(x, y):
     return -1
 
 def create_connection(i, j, ad_matrix):
-    ad_matrix[i, j] = 1
-    ad_matrix[j, i] = 1
+    ad_matrix[i, j] = not ad_matrix[i, j]
+    ad_matrix[j, i] = not ad_matrix[j, i]
+
+def remove_node(i):
+    ad_matrix[:i, i:-1] = ad_matrix[:i, i+1:]  # Shift top-right to the left
+    ad_matrix[i:-1, :i] = ad_matrix[i+1:, :i]  # Shift bottom-left upwards
+    ad_matrix[i:-1, i:-1] = ad_matrix[i+1:, i+1:]  # Shift bottom-right to top-left
+
+    # Fill the last row and column with zeros
+    ad_matrix[-1, :] = 0  # Last row
+    ad_matrix[:, -1] = 0  # Last column
+    nodes.pop(i)
 
 def save_graph(ad_matrix):
     graph_count = len(get_graph_filenames())
@@ -52,13 +67,18 @@ def read_adjacency_matrix(file_path):
     df = pd.read_csv(file_path, header=None)
     return df.values
 
-# if input("Read from existing graph? (y/N)") == "y":
-#     existing_graphs = get_graph_filenames() 
-#     for g in existing_graphs:
-#         print(g)
-#     n = int(input(f"Please select a graph (1-{len(existing_graphs)})"))
-#     m = read_adjacency_matrix(os.path.join(GRAPH_DIR, f"graph{n}.csv"))
-#     ad_matrix[:m.shape[0],:m.shape[1]] = m
+if input("Read from existing graph? (y/N)") == "y":
+    existing_graphs = get_graph_filenames() 
+    for g in existing_graphs:
+        print(g)
+    n = int(input(f"Please select a graph (1-{len(existing_graphs)})"))
+    m = read_adjacency_matrix(os.path.join(GRAPH_DIR, f"graph{n}.csv"))
+    ad_matrix[:m.shape[0],:m.shape[1]] = m
+    for i in range(m.shape[0]):
+        random.seed(i)
+        x = random.uniform(0, DIM[0])
+        y = random.uniform(0, DIM[1])
+        nodes.append([x, y])
 
 pygame.init()
 window = pygame.display.set_mode(DIM)
@@ -79,22 +99,23 @@ while True:
             if event.key == pygame.K_SPACE:
                 save_graph(ad_matrix)
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                selected_node = handle_click(pygame.mouse.get_pos(), selected_node, ad_matrix)
+            selected_node = handle_click(event.button, pygame.mouse.get_pos(), selected_node, ad_matrix)
 
     window.fill((25, 25, 25))
 
-    for g in range(MAX_NODES):
+    for i in range(MAX_NODES):
         for j in range(MAX_NODES):
-            if ad_matrix[g, j]:
-                pygame.draw.line(window, (128, 128, 128), nodes[g], nodes[j], EDGE_THICKNESS)
-
-    for g, node in enumerate(nodes):
-        if g == selected_node:
-            pygame.draw.circle(window, (255, 255, 255), node, NODE_SIZE + 3)
-        pygame.draw.circle(window, (128, 128, 128), node, NODE_SIZE)
+            if ad_matrix[i, j]:
+                pygame.draw.line(window, (128, 128, 128), nodes[i], nodes[j], EDGE_THICKNESS)
     
-
+    degrees = np.sum(ad_matrix, axis=0)
+    c = 2 / max(1, max(degrees))
+    for i, node in enumerate(nodes):
+        redness = int(255 / (1 + math.exp(-c * degrees[i])))
+        if i == selected_node:
+            pygame.draw.circle(window, (255, 255, 255), node, NODE_SIZE + 3)
+        pygame.draw.circle(window, (redness, 255 - redness, 255 - redness), node, NODE_SIZE)
+    
     pygame.display.update()
 
     clock.tick(20)
